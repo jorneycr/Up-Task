8a647f1a092b535206446d75d9d055a34715b2ec
jorneylm <jorney95@live.com>
2021-10-23 16:14:10 -0600

    reset email with token, and change your password with a security way.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,8 @@ const Usuarios = require('../models/Usuarios');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op
 const crypto = require('crypto');
+const bcrypt = require('bcrypt-nodejs');
+const enviarEmail = require('../handlers/email');
 
 //autentificar el usuario
 exports.autenticarUsuario = passport.authenticate('local', {
@@ -32,25 +34,38 @@ exports.cerrarSesion = (req, res) => {
 
 //crear token si el usuario es valido
 exports.enviarToken = async (req, res) => {
-    //verificar el usuario
-    const { email } = req.body;
-    const usuario = await Usuarios.findOne({ where: { email } });
-
-    //sino existe el usuario
-    if (!usuario) {
-        req.flash('error', 'No existe esa cuenta');
-        res.redirect('/restablecer');
-    }
-
-    //usuario existe
-    usuario.token = crypto.randomBytes(20).toString('hex');
-    usuario.expiration = Date.now() + 3600000;
-
-    //guardalos en la base de datos
-    await usuario.save();
-
-    //url de reset
-    const resetUrl = `http://${req.headers.host}/restablecer/${usuario.token}`;
+     // verificar que el usuario existe
+     const {email} = req.body
+     const usuario = await Usuarios.findOne({where: { email }});
+ 
+     // Si no existe el usuario
+     if(!usuario) {
+         req.flash('error', 'No existe esa cuenta');
+         res.redirect('/reestablecer');
+     }
+ 
+     // usuario existe
+     usuario.token = crypto.randomBytes(20).toString('hex');
+     usuario.expiration = Date.now() + 3600000;
+ 
+     // guardarlos en la base de datos
+     await usuario.save();
+ 
+     // url de reset
+     const resetUrl = `http://${req.headers.host}/restablecer/${usuario.token}`;
+ 
+     // Enviar el Correo con el Token
+ 
+     await enviarEmail.enviar({
+         usuario,
+         subject: 'Password Reset', 
+         resetUrl, 
+         archivo : 'restablecer-password'
+     });
+ 
+     // terminar
+     req.flash('correcto', 'Se envió un mensaje a tu correo');
+     res.redirect('/iniciar-sesion');
 
 }
 
@@ -61,35 +76,48 @@ exports.validarToken = async (req, res) => {
         }
     });
 
-    //sino encuentra al usuario
-    if(!usuario){
-        req.flash('error', 'No valido');
+    // sino encuentra el usuario
+    if(!usuario) {
+        req.flash('error', 'No Válido');
         res.redirect('/restablecer');
     }
 
-    // formulario para generar el password
-    res.render('resetPassword',{
-        nombrePagina: 'Restablecer Contraseña'
+    // Formulario para generar el password
+    res.render('resetPassword', {
+        nombrePagina : 'Restablecer Contraseña'
     })
-
 }
 
-//cambia el password por uno nuevo
-exports.actualizarPassword = async (req, res)=>{
+// cambia el password por uno nuevo
+exports.actualizarPassword = async (req, res) => {
 
-    //verifica el token valido y tambien la fecha es expiracion
+    // Verifica el token valido pero también la fecha de expiración
     const usuario = await Usuarios.findOne({
-        where:{
+        where: {
             token: req.params.token,
-            expiration:{
-                [Op.get] : Date.now()
+            expiration: {
+                [Op.gte] : Date.now()
             }
         }
     });
 
-    //verificamos si el usuario existe
-    if(!usuario){
-        req.flash('error', 'No valido');
+    // verificamos si el usuario existe
+    if(!usuario) {
+        req.flash('error', 'No Válido');
         res.redirect('/restablecer');
     }
+
+    // hashear el nuevo password
+
+    
+    usuario.token = null;
+    usuario.expiration = null;
+    usuario.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10) );
+    
+    // guardamos el nuevo password
+    await usuario.save();
+
+    req.flash('correcto', 'Tu password se ha modificado correctamente');
+    res.redirect('/iniciar-sesion');
+
 }
